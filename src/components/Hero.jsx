b3cf0e56c685254cf4dd67bf1d6ec09c6883ed8a
Dhrumil Trada <dhrumiltrada11@gmail.com>
2024-09-07 01:28:28 +0530

    minor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
         <div>
   <header className="header" data-header>
     <div className="container">
-      <a href="#" className="logo">
+      <a href="" className="logo">
         <ion-icon name="barbell-sharp" aria-hidden="true" />
         <span className="span">Fitlife</span>
       </a>
@@ -16,23 +16,23 @@ function Hero() {
         </button>
         <ul className="navbar-list">
           <li>
-            <a href="#home" className="navbar-link active" data-nav-link>Home</a>
+            <a href="" className="navbar-link active" data-nav-link>Home</a>
           </li>
           <li>
-            <a href="#about" className="navbar-link" data-nav-link>About Us</a>
+            <a href="" className="navbar-link" data-nav-link>About Us</a>
           </li>
           <li>
-            <a href="#class" className="navbar-link" data-nav-link>Classes</a>
+            <a href="" className="navbar-link" data-nav-link>Classes</a>
           </li>
           <li>
-            <a href="#blog" className="navbar-link" data-nav-link>Blog</a>
+            <a href="" className="navbar-link" data-nav-link>Blog</a>
           </li>
           <li>
-            <a href="#" className="navbar-link" data-nav-link>Contact Us</a>
+            <a href="" className="navbar-link" data-nav-link>Contact Us</a>
           </li>
         </ul>
       </nav>
-      <a href="#" className="btn btn-secondary">Join Now</a>
+      <a href="" className="btn btn-secondary">Join Now</a>
       <button className="nav-open-btn" aria-label="open menu" data-nav-toggler>
         <span className="line" />
         <span className="line" />
@@ -52,11 +52,11 @@ function Hero() {
             <p className="section-text">
               Duis mollis felis quis libero dictum vehicula. Duis dictum lorem mi, a faucibus nisi eleifend eu.
             </p>
-            <a href="#" className="btn btn-primary">Get Started</a>
+            <a href="" className="btn btn-primary">Get Started</a>
           </div>
           <div className="hero-banner">
             <img src="./src/assets/images/hero-banner.png" width={660} height={753} alt="hero banner" className="w-100" />
-            <img src="./src/assets/images/Phero-circle-one.png" width={666} height={666} aria-hidden="true" alt="" className="circle circle-1" />
+            <img src="./src/assets/images/hero-circle-one.png" width={666} height={666} aria-hidden="true" alt="" className="circle circle-1" />
             <img src="./src/assets/images/hero-circle-two.png" width={666} height={666} aria-hidden="true" alt="" className="circle circle-2" />
             <img src="./src/assets/images/heart-rate.svg" width={255} height={270} alt="heart rate" className="abs-img abs-img-1" />
             <img src="./src/assets/images/calories.svg" width={348} height={224} alt="calories" className="abs-img abs-img-2" />
