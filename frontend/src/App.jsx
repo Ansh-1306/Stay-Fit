6d546b5fbb3e7a68f93d3d56b49b2d658781fa79
Dhrumil Trada <dhrumiltrada11@gmail.com>
2024-09-19 13:49:28 +0530

    added protected Routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, } from "react-router-dom";
 import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
+import LoginPage from './templates/Login1'
 import SignUpPage from './pages/SignUpPage'
 import Data from "./templates/Data";
 import Fig from "./components/Fig";
+import Protected from "./components/Protected";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Protected Component={LandingPage} />} />
         <Route path="/data" element={<Data />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
---
+++ frontend/src/components/Protected.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Protected(props) {
+    const { Component } = props
+    const navigate = useNavigate() 
+    useEffect(() => {
+        let login = localStorage.getItem("user")
+        if(!login){
+            navigate("/login")
+        }
+    })
+
+  return (
+    <div>
+        <Component />
+    </div>
+  )
+}
+
+export default Protected
